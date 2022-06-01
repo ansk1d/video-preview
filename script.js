2a5d4187a60d737ea77a437e26a7467941a1cb27
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-06-01 15:32:38 +0300

    Did It!

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeVid(Rvid){
+Rvid.parentElement.parentElement.parentElement.querySelector('.vid').src =Rvid.src
+} 
+
+function changetitle(title){
+console.log(title.parentElement)
+}
